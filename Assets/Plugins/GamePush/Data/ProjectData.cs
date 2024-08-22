575b0cfb1114b1d2587824f5754c016ff34030de
Nikita Kiselev <nikita.k@danko.team>
2024-08-23 00:49:11 +0700

    GamePush SDK Added

---
+++ Assets/Plugins/GamePush/Data/ProjectData.cs
@@ -0,0 +1,10 @@
+namespace GamePush.Data
+{
+    public static class ProjectData
+    {
+        public static string SDK_VERSION = "1.6.0";
+        public static string ID = "16514";
+        public static string TOKEN = "pgVxnxE35i1xpKamttLkfe3Le5kMSPDV";
+        public static bool GAMEREADY_AUTOCALL = true;
+    }
+}
---
+++ Assets/Plugins/GamePush/JS/jspreData.txt
@@ -0,0 +1,4 @@
+const dataProjectId = '16514';
+const dataPublicToken = 'pgVxnxE35i1xpKamttLkfe3Le5kMSPDV';
+const showPreloaderAd = 'False';
+const autocallGameReady = 'True';
